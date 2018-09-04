b659e912809965413987307c1cbdf0d88dbc473c
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-04 11:43:26 -0700

    added first endpoint

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+
+
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);

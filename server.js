8e84b6dd1f80d0665676f5df7aa7783be3558e25
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-06 12:38:55 -0700

    added morgan logger, removed my old logger

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 'use strict';
 
-// Load array of notes
 const data = require('./db/notes');
 const simDB = require('./db/simDB'); 
 const notes = simDB.initialize(data);
 const { PORT } = require('./config');
-const logger = require('./middleware/logger');
-
-
-// INSERT EXPRESS APP CODE HERE...
-
+const morgan = require('morgan');
 const express = require('express');
+
+
 const app = express();
-app.use(logger.logger);
-// ADD STATIC SERVER HERE
+
+app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.json());
 
 
-
 app.get('/api/notes', (req, res, next) => {
   const { searchTerm } = req.query;
-
+  
   notes.filter(searchTerm, (err, list) => {
     if (err) {
-      return next(err); // goes to error handler
+      return next(err); 
     }
-    res.json(list); // responds with filtered array
+    res.json(list); 
   });
 });
 
@@ -56,7 +52,6 @@ app.get('/api/notes/:id', (req, res, next) => {
 app.put('/api/notes/:id', (req, res, next) => {
   const id = req.params.id;
 
-  /***** Never trust users - validate input *****/
   const updateObj = {};
   const updateFields = ['title', 'content'];
   console.log(req.body);

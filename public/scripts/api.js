cb8025fb475545c394af16449aaf05adad71d2ca
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-06 13:54:18 -0700

    finished with the feature/restful-routers branch

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -13,7 +13,16 @@ const api = {
     });
   },
 
-  update: function(id, obj, callback) {
+  details: function (id, callback) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: `/api/notes/${id}`,
+      success: callback
+    });
+  },
+
+  update: function (id, obj, callback) {
     $.ajax({
       type: 'PUT',
       url: `/api/notes/${id}`,
@@ -24,12 +33,25 @@ const api = {
     });
   },
 
-  details: function (id, callback) {
+  create: function (obj, callback) {
     $.ajax({
-      type: 'GET',
+      type: 'POST',
+      url: '/api/notes',
+      contentType: 'application/json',
       dataType: 'json',
+      processData: false,
+      data: JSON.stringify(obj),
+      success: callback
+    });
+  },
+
+  remove: function (id, callback) {
+    return $.ajax({
+      type: 'DELETE',
       url: `/api/notes/${id}`,
+      dataType: 'json',
       success: callback
     });
   }
+
 };
